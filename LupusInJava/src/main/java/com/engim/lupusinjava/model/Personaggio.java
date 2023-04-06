3b4639481f89bdc4687bc5856961b866825aeba6
fily1212 <fily1212@gmail.com>
2023-04-06 12:19:51 +0200

    costruttore Personaggio

--- LupusInJava/src/main/java/com/engim/lupusinjava/model/Personaggio.java
+++ LupusInJava/src/main/java/com/engim/lupusinjava/model/Personaggio.java
@@ -6,6 +6,14 @@ public abstract class Personaggio {
     private boolean votato;
     private boolean potereUsato;
 
+    public Personaggio(String ruolo, Fazione fazione) {
+        this.ruolo = ruolo;
+        this.vivo = true;
+        this.votato = false;
+        this.potereUsato = false;
+        this.fazione = fazione;
+    }
+
     private Fazione fazione;
 
     enum Fazione{BUONO,CATTIVO}
