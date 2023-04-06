fd24a55e077928281e58af82aa41b3879c0b6a3e
fily1212 <fily1212@gmail.com>
2023-04-06 11:21:45 +0200

    project

---
+++ LupusInJava/src/main/java/com/engim/lupusinjava/LupusInJavaApplication.java
@@ -0,0 +1,13 @@
+package com.engim.lupusinjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LupusInJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LupusInJavaApplication.class, args);
+    }
+
+}
---
+++ LupusInJava/src/test/java/com/engim/lupusinjava/LupusInJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engim.lupusinjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LupusInJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
